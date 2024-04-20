c9d8d4eea1ff470c965bf8c430e27bc7d013df09
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-04-21 06:06:07 +0900

    first commit

---
+++ public/info.php
@@ -0,0 +1,23 @@
+<?php
+const HOST_NAME = 'mysql_test';
+const HOST_PORT = '3306';
+const DB_NAME   = 'testdb';
+const USER_NAME = 'testuser';
+const PASSWORD  = '1234567890';
+
+$db_settings = [
+	'host='.HOST_NAME,
+	'port='.HOST_PORT,
+	'dbname='.DB_NAME,
+	'charset=utf8'
+];
+
+try {
+	$db = new PDO('mysql:'.implode(';', $db_settings), USER_NAME, PASSWORD);
+	echo '接続成功';
+} catch (PDOException $e) {
+	echo '接続失敗：'.$e->getMessage();
+}
+?>
+
+<?php phpinfo(); ?>
