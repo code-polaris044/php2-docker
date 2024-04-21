6ab62f0eb870a9edca1c44f9fe8c9d47744c618f
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-04-21 22:57:35 +0900

    fix: Update the ib_buffer_pool file with new data
    Update the ib_buffer_pool file to reflect the changes in the database. 
    Removed old data and added new data entries for better performance.

--- public/index.php
+++ public/index.php
@@ -6,11 +6,52 @@
     <title>Document</title>
 </head>
 <body>
+
+
+
     <form action="test.php" method="post">
         氏名: <input type="text" name="username" >
         <input type="submit" value="送信">
     </form>
 
     <?php echo date("Y-m-d H-i-s"); ?>
+
+    <form action="test.php" method="post">
+        数字1: <input type="text" name="val1"><br>
+        数字2: <input type="text" name="val2">
+        <input type="submit" value="結果">
+    </form>
+
+    <?php 
+    $a = array("りんご","みかん","ぶどう","ばなな","こんにゃく");
+
+    for($i = 0; $i < count($a); $i++) {
+    echo $a[$i] . "<br>";
+    }
+
+    ?>
+
+<form action="test.php" method="post">
+    <input type="text" name="english"><br>
+    <input type="submit" value="変換">
+</form>
+
+
+<form action="test.php" method="post">
+    郵便番号を入力してください: 
+    <input type="text" name="yubin" id=""><br>
+    <input type="submit" value="チェック">
+</form>
+
+
+<form action="test.php" method="post">
+    メールアドレスを入力してください: 
+    <input type="text" name="email" id=""><br>
+    <input type="submit" value="チェック">
+</form>
+
+
+
+
 </body>
 </html>
